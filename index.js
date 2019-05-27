2adb2854f74d2e313e6dac4f257d8ce5199e300a
Mathis Hofer <hofer@puzzle.ch>
2019-05-27 15:46:26 +0200

    Proxy requests to /Files/personPictures/:id to placeimg.com

--- index.js
+++ index.js
@@ -17,20 +17,35 @@ const transformations = {
 
 const app = express();
 
-app.use(
-  cache('30 minutes'),
-  proxy(MOCK_API_URL, {
-    userResDecorator: (proxyRes, proxyResData, userReq, userRes) => {
-      const { pathname } = url.parse(userReq.url);
-      if (Object.keys(transformations).includes(pathname)) {
-        const input = JSON.parse(proxyResData);
-        const output = transformations[pathname](userReq, input);
-        return JSON.stringify(output);
-      }
-      return proxyResData;
+const proxyImages = proxy('https://placeimg.com', {
+  filter: (req, res) =>
+    req.method == 'GET' &&
+    /^\/Files\/personPictures\/[0-9]+$/.test(url.parse(req.url).pathname),
+  proxyReqPathResolver: req => '/100/130/people',
+  userResDecorator: (proxyRes, proxyResData, userReq, userRes) => {
+    // Return a 404 for 10% of the images
+    if (Math.random() < 0.1) {
+      userRes.status(404);
+      userRes.contentType('text/html');
+      return 'Image not found';
     }
-  })
-);
+    return proxyResData;
+  }
+});
+
+const proxyMockApi = proxy(MOCK_API_URL, {
+  userResDecorator: (proxyRes, proxyResData, userReq, userRes) => {
+    const { pathname } = url.parse(userReq.url);
+    if (Object.keys(transformations).includes(pathname)) {
+      const input = JSON.parse(proxyResData);
+      const output = transformations[pathname](userReq, input);
+      return JSON.stringify(output);
+    }
+    return proxyResData;
+  }
+});
+
+app.use(cache('30 minutes'), proxyImages, proxyMockApi);
 
 app.listen(PORT, () =>
   console.log(`Listening on http://localhost:${PORT} ...`)
