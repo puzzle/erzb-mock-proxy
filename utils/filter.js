ef7ab7e17eb6e1d0e3ce7b907069e52bb4dc55cd
Claudia Asti <asti@puzzle.ch>
2019-05-28 11:46:09 +0200

    Add null check on filter param

--- utils/filter.js
+++ utils/filter.js
@@ -1,6 +1,6 @@
 function getFilterValue(req, field, operator) {
   const paramValue = req.query[`filter.${field}`];
-  const matches = paramValue.match(`${operator}(.*)`);
+  const matches = paramValue && paramValue.match(`${operator}(.*)`);
   return (matches && matches[1]) || null;
 }
 
